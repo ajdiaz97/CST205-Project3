b5c6be5561d430d942ad91d0eaa72e6fe4b26dd2
cruzbm <brcruz@csumb.edu>
2016-05-11 14:10:14 -0700

    Add files via upload
    Initial start page with nothing more than the function to close itself. Having issues because it's generating a second window unexpectedly.

---
+++ AlienStartPage.py
@@ -0,0 +1,56 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+LARGE_FONT = ("Verdana", 12)
+self = Tk()
+
+
+
+class FirstApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+
+                       
+        container = tk.Frame(self)
+
+        container.pack(side = "top", fill = "both", expand = True)
+
+        container.grid_rowconfigure(0, weight = 1)
+        container.grid_columnconfigure(0, weight = 1)
+
+        self.frames = {}
+
+        for F in (StartPage, StartPage):
+
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row = 0, column= 0, sticky = "nsew")
+
+        self.show_frame(StartPage)
+
+        def CloseWindow():
+            exit()
+
+        buttonClose = Button(self, text="Close", command=CloseWindow)
+        buttonClose.pack()
+        
+    def show_frame(self, cont):
+        
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="UFO SIGHTINGS IN CALIFORNIA", font=LARGE_FONT)
+        label.pack(pady=100,padx=100)
+
+        button1 = tk.Button(self, text = "Shape Button",
+                            command = lambda: qf("see if this worked!"))
+
+
+
+app = FirstApp()
+app.mainloop()
