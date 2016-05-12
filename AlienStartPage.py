e39be39ea2902a91aeec781e9a8c31f856e7c646
cruzbm <brcruz@csumb.edu>
2016-05-12 11:27:18 -0700

    Update AlienStartPage.py
    Widgets have been included for each page, get functions will be added.

--- AlienStartPage.py
+++ AlienStartPage.py
@@ -4,75 +4,123 @@
 
 import tkinter as tk
 
-class UFO(tk.Tk):
-    
-    def __init__(self, *args, **kwargs):
 
+LARGE_FONT= ("Helvetica", 44)
+
+
+class UFO(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        
         tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
 
-        container.pack(side = "top", fill = "both", expand = True)
+        container.pack(side="top", fill="both", expand = True)
 
-        container.grid_rowconfigure(0, weight = 1)
-        container.grid_columnconfigure(0, weight = 1)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
         for F in (StartPage, CityPage, ShapePage, DatePage):
-            
-            frame = F(container,self)
-            
+
+            frame = F(container, self)
+
             self.frames[F] = frame
-            
-            frame.grid(row = 0, column = 0, sticky = "nsew")
+
+            frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
 
-        frame=self.frames[cont]
+        frame = self.frames[cont]
         frame.tkraise()
 
+        
 class StartPage(tk.Frame):
-    
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent,controller)
-        label = tk.Label(self, text = "UFO SIGHTINGS IN CALIFORNIA", font = "Helvetica 44")
-        label.pack(pady = 10, padx = 10)
 
-        button = tk.Button(self, text = "City Search", command = lambda: controller.show_frame(CityPage))
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        #button to city search
+        button = tk.Button(self, text="City Search",
+                            command=lambda: controller.show_frame(CityPage))
         button.pack()
 
-        button2 = tk.Button(self, text = "Shape Search", command = lambda: controller.show_frame(ShapePage))
+        #button to shape search
+        button2 = tk.Button(self, text="Shape Search",
+                            command=lambda: controller.show_frame(ShapePage))
         button2.pack()
 
-        button3 = tk.Button(self, text = "Date Search", command=lambda:controller.show_frame(DatePage))
+        #button to date search
+        button3 = tk.Button(self, text="Date Search",
+                            command=lambda: controller.show_frame(DatePage))
         button3.pack()
-        
+
+
 class CityPage(tk.Frame):
+
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text = "City Search", font = "Helvetica 44")
-        label.pack(pady = 10, padx = 10)
+        label = tk.Label(self, text="City Search", font=LARGE_FONT)
+        label.pack()
 
-        button1 = tk.Button(self, text = "DO NOTHING")
-        button1.pack()
+        #creating city search widget
+        cityEntry = tk.Entry(self) #search bar is appearing everywhere
+        cityEntry.config(width = 15,font = "Helvetica 28")
+        cityEntry.pack()
 
 class ShapePage(tk.Frame):
-    tk.Frame.__init__(self, parent, controller)
-    label = tk.Label(self,text= "Shape Search", font = "Helvetica 44")
-    label.pack(pady=10, padx=10)
 
-    button = tk.Button(self, text="DO NOTHING")
-    button.pack()
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Shape Search", font=LARGE_FONT)
+        label.grid(row = 0, column = 1)
 
+        #creating shape search widget
+        #Using listbox for shape search for only 18
+        shapeList = tk.Listbox(self, font = "Helvetica 16")
+        shapeList.insert(1, "Chevron")
+        shapeList.insert(2, "Cigar")
+        shapeList.insert(3, "Circle")
+        shapeList.insert(4, "Cone")
+        shapeList.insert(5, "Cross")
+        shapeList.insert(6, "Cylinder")
+        shapeList.insert(7, "Diamond")
+        shapeList.insert(8, "Disk")
+        shapeList.insert(9, "Unknown")
+        shapeList.insert(10, "Egg")
+        shapeList.insert(11, "Fireball")
+        shapeList.insert(12, "Formation")
+        shapeList.insert(13, "Light")
+        shapeList.insert(14, "Other")
+        shapeList.insert(15, "Oval")
+        shapeList.insert(16, "Rectangle")
+        shapeList.insert(17, "Sphere")
+        shapeList.insert(18, "Triangle")
+        shapeList.grid(row = 1, column = 1)
+        
+        
 class DatePage(tk.Frame):
-    tk.Frame.__init__(self, parent, controller)
-    label = tk.Label(self,text="Date Search", font = "Helvetica 44")
-    label.pack(pady=10,padx=10)
 
-    button = tk.Button(self, text="DO NOTHING")
-    button.pack()
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Date Search", font=LARGE_FONT)
+        label.pack()
 
-root = UFO()
-root.mainloop()
+        blankSpace = tk.Label(self, text = "")
+        blankSpace.pack()
+
+        #creating spinbox for date
+        #creating spinbox to select date by first month, then day
+        monthLabel = tk.Label(self, text = "Month", font = "Helvetica 12").pack()
+        month = tk.Spinbox(self, from_=1, to=12, font = "Helvetica 16").pack()
+        dayLabel = tk.Label(self, text = "Day", font = "Helvetica 12").pack()
+        day = tk.Spinbox(self, from_=1, to=31, font = "Helvetica 16").pack()
+        
+        
+app = UFO()
+app.mainloop()
